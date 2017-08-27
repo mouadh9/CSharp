12569779055350c4fe52db2fde0ea52a9a9057d7
mouadh ben salem <mouadh.bensalem@716solutions.com>
2017-08-27 23:32:22 +0100

    Write a program that can store a list of directions and then print out the list as a series of symbols.

--- TestConnectionToGithub/Program.cs
+++ TestConnectionToGithub/Program.cs
@@ -6,12 +6,68 @@ using System.Threading.Tasks;
 
 namespace TestConnectionToGithub
 {
+    //enum Direction { North, South, East, West }
     class Program
     {
         static void Main(string[] args)
         {
-            // this for testing
-            Console.WriteLine("mouadh ben salem");
+            //List<Direction> directions = new List<Direction>();
+            List<string> directions = new List<string>();
+
+            char input;
+
+            do
+            {
+                Console.WriteLine("Enter the next direction (n/s/e/w or 0)");
+                input = Console.ReadKey().KeyChar;
+                Console.ReadLine();
+
+                switch (input)
+                {
+                    case 'n':
+                        directions.Add("North");
+                        break;
+                    case 's':
+                        directions.Add("South");
+                        break;
+                    case 'e':
+                        directions.Add("East");
+                        break;
+                    case 'w':
+                        directions.Add("West");
+                        break;
+                    case '0':
+                        break;
+                    default:
+                        Console.WriteLine("Error Input");
+                        break;
+                }
+            } while (input != '0');
+            Console.Write($"\nYou Entred {directions.Count} Element");
+
+            foreach (var direction in directions)
+            {
+                Console.WriteLine($"{DirectionToSymbol(direction)}");
+            }
+            Console.ReadKey();
+        }
+        static string DirectionToSymbol(string direction)
+        {
+            switch (direction)
+            {
+                case "North":
+                    return "fou9";
+                case "South":
+                    return "louta";
+                case "East":
+                    return "<";
+                case "West":
+                    return ">";
+                default:
+                    return "";
+
+
+            }
         }
     }
 }
